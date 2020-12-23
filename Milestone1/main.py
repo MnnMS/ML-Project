dba9232f0abad0f1470dad9bdb41ab744ecdfc8a
MohamedAdel769 <mohamed.adel90211@gmail.com>
2020-12-23 08:21:09 +0200

    Model done

--- Milestone1/main.py
+++ Milestone1/main.py
@@ -1,11 +1,41 @@
 from pre_processing import *
 import pandas as pd
 import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
+from sklearn.model_selection import train_test_split
+from sklearn import linear_model
+from sklearn import metrics
 
 data = pd.read_csv('AppleStore_training.csv')
+
 data.dropna(how='any',inplace=True)
+data.drop(columns=['currency', 'track_name'], inplace=True)
+# all_data = data.iloc[:,:]
 X=data.iloc[:,0:15]
 Y=data['user_rating']
-cols=['prime_genre','currency','track_name','ver','cont_rating']
+
+cols=['prime_genre','ver','cont_rating']
 X = One_Hot_Encoding(X,cols)
-X = featureScaling(np.array(X),0,1);
+X = featureScaling(np.array(X),0,1)
+
+X_train, X_test, y_train, y_test = train_test_split(X, Y, test_size = 0.30, shuffle=True)
+
+# corr = all_data.corr()
+# top_feature = corr.index[abs(corr['user_rating']>0.3)]
+# top_corr = all_data[top_feature].corr()
+
+cls = linear_model.LinearRegression()
+cls.fit(X_train,y_train)
+prediction= cls.predict(X_test)
+
+print('Mean Square Error', metrics.mean_squared_error(np.asarray(y_test), prediction))
+
+true_rate_value=np.asarray(y_test)[5]
+predicted_rate_value=prediction[5]
+
+print('True rate value s is : ' + str(true_rate_value))
+print('Predicted rate value  is : ' + str(predicted_rate_value))
+
+
+
--- Milestone1/pre_processing.py
+++ Milestone1/pre_processing.py
@@ -1,17 +1,21 @@
 import pandas as pd
 import numpy as np
+
 def One_Hot_Encoding(X,cols):
     for c in cols:
         # get one hot encoding for X[c] column
         df2 = pd.get_dummies(X[c])
         #drop X[c] column
-        X.drop(c,axis=1, inplace=True)
+        X.drop(columns=[c],axis=1, inplace=True)
         #add new columns into df
         X = pd.concat([X, df2], axis=1)
     return X
 
 def featureScaling(X,a,b):
-    Normalized_X=np.zeros((X.shape[0],X.shape[1]));
+    Normalized_X=np.zeros((X.shape[0],X.shape[1]))
     for i in range(X.shape[1]):
-        Normalized_X[:,i]=((X[:,i]-min(X[:,i]))/(max(X[:,i])-min(X[:,i])))*(b-a)+a;
+        mx = max(X[:,i])
+        mn = min(X[:,i])
+        if mx != mn:
+            Normalized_X[:,i]=((X[:,i]-min(X[:,i]))/(mx-mn))*(b-a)+a
     return Normalized_X
